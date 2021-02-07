fe79ded598248c30b1117f67d2ada1be0dd22400
KoRnelius-87 <70422966+KoRnelius-87@users.noreply.github.com>
2021-02-06 21:43:36 -0800

    Update Main.java
    Parte visual y creacion de clases por metodo

--- MD_imtegrador/src/main/java/com/mycompany/md_imtegrador/Main.java
+++ MD_imtegrador/src/main/java/com/mycompany/md_imtegrador/Main.java
@@ -1,13 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.md_imtegrador;
-
 /**
  *
- * @author hpave
+ * @author hpave, KoRnelius87
  */
 public class Main {
     
