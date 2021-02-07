708186cb9ce60a0e74673c324058e21ab8fb7e65
KoRnelius-87 <70422966+KoRnelius-87@users.noreply.github.com>
2021-02-06 21:54:22 -0800

    Revert "eliminar main"
    This reverts commit 7d6f624dd402de541272aba9cdfbee5041bf7a4a.

---
+++ MD_integrador/src/main/java/com/mycompany/md_integrador/Main.java
@@ -0,0 +1,13 @@
+package com.mycompany.md_integrador;
+/**
+ *
+ * @author hpave, KoRnelius87
+ */
+public class Main {
+    
+    
+    
+    public static void main(String[] args) {
+        
+    }
+}
