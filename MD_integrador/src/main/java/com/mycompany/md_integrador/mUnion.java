bf3f77aeb9b630001ead6b9fb8c5918b19fad8bf
Hector <52730210+Hector954@users.noreply.github.com>
2021-02-07 23:21:29 -0800

    Metodo Union
    Se implemento el metodo unir conjuntos, y se asigno a los botones las acciones correspondientes

--- MD_integrador/src/main/java/com/mycompany/md_integrador/mUnion.java
+++ MD_integrador/src/main/java/com/mycompany/md_integrador/mUnion.java
@@ -1,14 +1,83 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.md_integrador;
 
 /**
  *
  * @author Armando
+ * @author Hector
  */
 public class mUnion {
-    
+
+    private String[] arrayA;
+    private String[] arrayB;
+    private String[] result;
+
+    private int sizeA;
+    private int sizeB;
+
+    public void ConjuntoCa(int sizeA) {
+        this.sizeA = sizeA;
+        arrayA = new String[sizeA];
+    }
+
+    public void ConjuntoCb(int sizeB) {
+        this.sizeB = sizeB;
+        arrayB = new String[sizeB];
+    }
+
+    public void setCa(String Ca, int i) {
+        arrayA[i] = Ca;
+    }
+
+    public void setCb(String Cb, int i) {
+        arrayB[i] = Cb;
+    }
+
+    public int getSizeA() {
+        return sizeA;
+    }
+
+    public int getSizeB() {
+        return sizeB;
+    }
+
+    public String getCa(int i) {
+        return arrayA[i];
+    }
+
+    public String getCb(int i) {
+        return arrayB[i];
+    }
+
+    public String unir() {
+        int count = 0;
+        int aux = 0;
+        String imprimir = "";
+
+        result = new String[sizeA + sizeB];
+        for (int i = 0; i < sizeA; i++) {
+            result[i] = arrayA[i];
+        }
+
+        count = sizeA;
+
+        for (int i = 0; i < sizeB; i++) {
+            aux = 0;
+            for (int j = 0; j < sizeA + sizeB; j++) {
+                if (arrayB[i].equals(result[j])) {
+                    aux = 1;
+                }
+            }
+            if (aux == 0) {
+                result[count] = arrayB[i];
+                count++;
+            }
+        }
+        
+        for (int i = 0; i < sizeA + sizeB; i++) {
+            if (result[i] != null) {
+                imprimir = imprimir + result[i] + ";";
+            }
+        }
+        return imprimir;
+    }
 }
