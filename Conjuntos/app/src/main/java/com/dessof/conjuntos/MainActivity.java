32bd9dff44e6817b21bd687fbfe3157ea2f1a970
Hector <52730210+Hector954@users.noreply.github.com>
2021-02-15 13:18:51 -0800

    Migracion finalizada
    Unicamente sobran algunos detalles sobre tooltips y poner falta logo cesun

---
+++ Conjuntos/app/src/main/java/com/dessof/conjuntos/MainActivity.java
@@ -0,0 +1,132 @@
+package com.dessof.conjuntos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.johnpersano.supertoasts.library.Style;
+import com.github.johnpersano.supertoasts.library.SuperActivityToast;
+
+public class MainActivity<cR> extends AppCompatActivity {
+    operadores op = new operadores();
+    private Button btnmU;
+    private Button btnmI;
+    private Button btnmD;
+    private Button btnmDs;
+    private Button btnClean;
+
+    private EditText conjuntoA;
+    private EditText conjuntoB;
+    private TextView conjuntoR;
+
+    public String[] splitA() {
+        String[] aA = conjuntoA.getText().toString().split(",");
+        return aA;
+    }
+
+    public String[] splitB() {
+        String[] aB = conjuntoB.getText().toString().split(",");
+        return aB;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        conjuntoA = (EditText) findViewById(R.id.cA);
+        conjuntoB = (EditText) findViewById(R.id.cB);
+        conjuntoR = (TextView) findViewById(R.id.cR);
+     // Boton limpiar
+     btnClean = (Button) findViewById(R.id.clean);
+
+     btnClean.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             conjuntoA.setText("");
+             conjuntoB.setText("");
+             conjuntoR.setText("");
+             conjuntoA.requestFocus();
+         }
+     });
+
+
+    // Metodo Unir
+        btnmU = (Button) findViewById(R.id.btnmU);
+
+        btnmU.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (conjuntoA.getText().length() == 0 || conjuntoB.getText().length() == 0) {
+                    Toast.makeText(MainActivity.this,"Verificar llenado de los conjuntos",Toast.LENGTH_LONG).show();
+                } else
+                    conjuntoR.setText("A U B = {" + op.unir(splitA(), splitB()) + "}");
+            }
+        });
+
+        // Metodo Interseccion
+        btnmI = (Button) findViewById(R.id.mI);
+
+        btnmI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (conjuntoA.getText().length() == 0 || conjuntoB.getText().length() == 0) {
+                    Toast.makeText(MainActivity.this,"Verificar llenado de los conjuntos",Toast.LENGTH_LONG).show();
+                } else
+                    conjuntoR.setText("A ∩ B  ó  B ∩ A = {" + op.interseccion(splitA(), splitB()) + "}");
+            }
+        });
+
+        // Metodo Diferencia
+          btnmD = (Button) findViewById(R.id.mD);
+
+        btnmD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (conjuntoA.getText().length() == 0 || conjuntoB.getText().length() == 0) {
+                    Toast.makeText(MainActivity.this,"Verificar llenado de los conjuntos",Toast.LENGTH_LONG).show();
+                } else
+                    conjuntoR.setText("A Δ B = {" + op.DiferenciaSimetrica(splitA(), splitB()) + "}");
+            }
+        });
+
+        // Metodo Diferencia Simetrica
+
+         btnmDs = (Button) findViewById(R.id.mDs);
+
+        btnmDs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (conjuntoA.getText().length() == 0 || conjuntoB.getText().length() == 0) {
+                    Toast.makeText(MainActivity.this,"Verificar llenado de los conjuntos",Toast.LENGTH_LONG).show();
+                } else
+                    conjuntoR.setText("A-B = {" + op.DiferenciaA(splitA(), splitB())
+                            + "}\nB-A = {" + op.DiferenciaB(splitA(), splitB()) + "}");
+            }
+        });
+
+
+
+
+
+
+
+
+
+        SuperActivityToast.create(this, new Style(), Style.TYPE_BUTTON)
+                .setText("By Héctor Páramo ft Armando Moran")
+                .setDuration(Style.DURATION_LONG)
+                .setColor(getColor(R.color.accent))
+                .setTextColor(getColor(R.color.black))
+                .setAnimations(Style.ANIMATIONS_FLY).show();
+
+
+    }
+
+
+}
---
+++ Conjuntos/app/src/main/java/com/dessof/conjuntos/operadores.java
@@ -0,0 +1,163 @@
+package com.dessof.conjuntos;
+
+public class operadores {
+
+    private String[] result;
+
+    // metodo unir
+    public String unir(String[] arrayA, String[] arrayB) {
+        int count = 0;
+        int aux = 0;
+        String imprimir = "";
+
+
+        result = new String[arrayA.length + arrayB.length];
+        for (int i = 0; i < arrayA.length; i++) {
+            result[i] = arrayA[i];
+        }
+
+        count = arrayA.length;
+
+        for (int i = 0; i < arrayB.length; i++) {
+            aux = 0;
+            for (int j = 0; j < arrayA.length + arrayB.length; j++) {
+                if (arrayB[i].equals(result[j])) {
+                    aux = 1;
+                }
+            }
+            if (aux == 0) {
+                result[count] = arrayB[i];
+                count++;
+            }
+        }
+
+        for (int i = 0; i < arrayA.length + arrayB.length; i++) {
+            if (result[i] != null) {
+                imprimir = imprimir + result[i] + ",";
+            }
+        }
+        return imprimir.substring(0, imprimir.length()-1);
+    }
+
+    // metodo inserccion
+    public String interseccion(String[] arrayA, String[] arrayB) {
+
+        result = new String[Math.max(arrayA.length, arrayB.length)];
+        String imprimir = " ";
+
+        for (int i = 0; i < arrayA.length; i++) {
+            for (int j = 0; j < arrayB.length; j++) {
+                if (arrayA[i].equals(arrayB[j])) {
+                    result[i] = arrayA[i];
+                }
+            }
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            if (result[i] != null) {
+                imprimir = imprimir + result[i] + ",";
+            }
+        }
+        return imprimir.substring(0, imprimir.length()-1);
+    }
+
+    // Metodo diferencia
+    public String DiferenciaA(String[] arrayA, String[] arrayB) {
+
+        int count = 0;
+        int aux = 0;
+        String imprimir = "";
+        // crea un arreglo con el tamano de los otros dos
+        result = new String[arrayA.length + arrayB.length];
+        // Compara ambos arreglos y busca los no repetidos en A
+        for (int i = 0; i < arrayA.length; i++) {
+            aux = 0;
+            for (int j = 0; j < arrayB.length; j++) {
+                if (arrayA[i].equals(arrayB[j])) {
+                    aux = 1;
+                }
+            }
+            if (aux == 0) {
+                result[count] = arrayA[i];
+                count++;
+            }
+        }
+        // salida en pantalla
+        for (int i = 0; i < arrayA.length + arrayB.length; i++) {
+            if (result[i] != null) {
+                imprimir = imprimir + result[i] + ",";
+            }
+        }
+        return imprimir.substring(0, imprimir.length()-1);
+    }
+
+    public String DiferenciaB(String[] arrayA, String[] arrayB) {
+
+        int count = 0;
+        int aux = 0;
+        String imprimir = "";
+
+        result = new String[arrayA.length + arrayB.length];
+
+        // No repetidos en B
+        for (int i = 0; i < arrayB.length; i++) {
+            aux = 0;
+            for (int j = 0; j < arrayA.length; j++) {
+                if (arrayB[i].equals(arrayA[j])) {
+                    aux = 1;
+                }
+            }
+            if (aux == 0) {
+                result[count] = arrayB[i];
+                count++;
+            }
+        }
+
+        for (int i = 0; i < arrayA.length + arrayB.length; i++) {
+            if (result[i] != null) {
+                imprimir = imprimir + result[i] + ",";
+            }
+        }
+        return imprimir.substring(0, imprimir.length()-1);
+    }
+
+    // diferencia simetrica
+    public String DiferenciaSimetrica(String[] arrayA, String[] arrayB){
+        int count=0;
+        int aux=0;
+
+        String imprimir=""; //Salida de Pantalla
+        result=new String [arrayA.length+arrayB.length];//Llena de Nullos Conjunto Resultante
+        //Los no Repetidos de A
+        for (int i=0;i<arrayA.length;i++){
+            aux=0;
+            for (int j=0;j<arrayB.length;j++){
+                if (arrayA[i].equals(arrayB[j]))
+                    aux=1;
+            }
+            if (aux==0){
+                result[count]=arrayA[i];
+                count++;
+            }
+        }
+        //Los no Repetidos de B
+        for (int i=0;i<arrayB.length;i++){
+            aux=0;
+            for (int j=0;j<arrayA.length;j++){
+                if (arrayB[i].equals(arrayA[j]))
+                    aux=1;
+            }
+            if (aux==0){
+                result[count]=arrayB[i];
+                count++;
+            }
+        }
+        //Llena String para Salida en Pantalla
+        for (int i=0;i<arrayA.length+arrayB.length;i++){
+            if (result[i]!=null)
+                imprimir=imprimir+result[i]+",";
+        }
+
+        return imprimir.substring(0, imprimir.length()-1);
+    }
+}
